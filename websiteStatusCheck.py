01e18a79d364b43637974ffebd7df4551520b1d6
Nicolas Cova <nicolas.cova.work@gmail.com>
2017-01-17 21:04:32 -0400

    Another insignificant commit
    Just fooling aroung with colors on the terminal and trying out a timed loop.

--- websiteStatusCheck.py
+++ websiteStatusCheck.py
@@ -3,16 +3,18 @@ import sys
 import urllib.request
 from pathlib import Path
 import json
+import time
 
 REQUEST_TIMEOUT = 10
+VERBOSE_MODE = False
 
 def read_sites_json(file_path):
     """Checks if the site's JSON file exists and reads it. Returns
     a Dictionary with (Site Name, Site Url) pairs."""
     sites_file = Path(file_path)
     if sites_file.exists() and sites_file.is_file():
-        with sites_file.open() as f:
-            contents = f.read()
+        with sites_file.open() as temp_file:
+            contents = temp_file.read()
             decoded = json.loads(contents)
             return decoded
     return None
@@ -26,14 +28,14 @@ def check_site_status(site_url):
         print("Codigo recibido: {0}".format(myrequest.getcode()))
     except urllib.error.HTTPError as ex1:
         if hasattr(ex1, "reason"):
-            print("We failed to reach a server.")
+            print("\x1b[0;37;41m" + " Warning " + "\x1b[0m" + " We failed to reach a server.")
             print("Reason: ", ex1.reason)
         if hasattr(ex1, "code"):
-            print("The server couldn\'t fulfill the request.")
+            print("\x1b[0;37;41m" + " Warning " + "\x1b[0m" + "The server couldn\'t fulfill the request.")
             print("Error code: ", ex1.code)
     except urllib.error.URLError as ex2:
         if hasattr(ex2, "reason"):
-            print("We failed to reach a server.")
+            print("\x1b[0;37;41m" + " Warning " + "\x1b[0m" + "We failed to reach a server.")
             print("Reason: ", ex2.reason)
 
 def main(argv):
@@ -42,8 +44,11 @@ def main(argv):
     # Create a list of sites to check
     sites_dict = read_sites_json('sites.json')
 
-    for key in sites_dict:
-        check_site_status(sites_dict[key])
+    if sites_dict is not None:
+        while True:
+            for key in sites_dict:
+                check_site_status(sites_dict[key])
+            time.sleep(30)
 
     sys.exit()
 
